c29d58a84ea3b474ca4f63c66b0f34a8bd852ad0
rizahudan <rizahudan@gmail.com>
2019-04-15 20:39:27 +0700

    fix app

--- src/imports/startups/client/index.js
+++ src/imports/startups/client/index.js
@@ -1,4 +1,4 @@
 import { render } from 'react-dom';
-import routes from '../../routes/route';
+import routes from '../../routes/client/route';
 
 render(routes(), document.getElementById('root'));
--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,26 @@
+import User from 'imports/classes/User';
+
 const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 
 const app = express();
+const PORT = process.env.PORT || 8080;
+// mongoose.connect('mongodb://192.168.137.1/nasional_leader_school', { useNewUrlParser: true });
+mongoose.connect('mongodb://127.0.0.1:3001/meteor', { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
-app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: 'hudan' }));
-app.listen(8080, () => {
-  mongoose.connect('mongodb://192.168.137.1/nasional_leader_school', { useNewUrlParser: true });
-  console.log('Listening on port 8080!');
+app.use(express.static('.build/client'));
+
+require('imports/routes/server/index.js')(app);
+
+app.get('/getUsername', (req, res) => res.send({ username: 'hudan' }));
+app.listen(PORT, () => {
+  const test = new User({ name: 'test save' });
+  test.save();
+  console.log(`test Listening on port ${PORT}!`);
 });
