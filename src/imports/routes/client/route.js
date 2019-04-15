26f269de3b8e75c1ecc749d729ff5718140eea08
rizahudan <rizahudan@gmail.com>
2019-04-15 20:38:27 +0700

    split route client

--- src/imports/routes/client/route.js
+++ src/imports/routes/client/route.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createBrowserHistory } from 'history';
 import { Router, Route, Switch } from 'react-router-dom';
-import Home from '../ui/home';
+import Home from 'imports/ui/home';
 
 const browserHistory = createBrowserHistory();
 
