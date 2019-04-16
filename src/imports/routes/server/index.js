d66c916403438350f1e42cb28eaab3962b4d922c
rizahudan <rizahudan@gmail.com>
2019-04-17 02:34:48 +0700

    implement route

--- src/imports/routes/server/index.js
+++ src/imports/routes/server/index.js
@@ -1,6 +1,10 @@
 import task from './task';
+import step from './step';
+import user from './user';
 
 const routes = (app) => {
   app.use('/task', task);
+  app.use('/step', step);
+  app.use('/user', user);
 };
 export default routes;
